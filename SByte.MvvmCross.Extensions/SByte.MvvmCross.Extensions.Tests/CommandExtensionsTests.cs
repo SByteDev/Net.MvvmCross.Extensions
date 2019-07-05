328d96030ed6a3fc40b43bb6c9298f5d6b76222a
Denys Fiediaiev <fiediaiev@sbyte.dev>
2019-07-05 14:54:32 +0300

    Add relay on extension

--- SByte.MvvmCross.Extensions/SByte.MvvmCross.Extensions.Tests/CommandExtensionsTests.cs
+++ SByte.MvvmCross.Extensions/SByte.MvvmCross.Extensions.Tests/CommandExtensionsTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using MvvmCross.Commands;
@@ -116,6 +118,130 @@ namespace SByte.MvvmCross.Extensions.Tests
                         var result = await command.SafeExecuteAsync(parameter).ConfigureAwait(false);
 
                         Assert.True(result);
+                    }
+                }
+            }
+        }
+
+        [TestFixture]
+        public class WhenRelayOnCalled
+        {
+            [TestFixture]
+            public class AndTheCommandIsNull
+            {
+                [Test]
+                public void ExceptionShouldBeThrown()
+                {
+                    Assert.Throws<ArgumentNullException>(() => default(ICommand).RelayOn(null));
+                }
+            }
+
+            [TestFixture]
+            public class AndTheNotifyPropertyChangedIsNull
+            {
+                [Test]
+                public void ExceptionShouldBeThrown()
+                {
+                    var command = Substitute.For<ICommand>();
+
+                    Assert.Throws<ArgumentNullException>(() => command.RelayOn(null));
+                }
+            }
+
+            [TestFixture]
+            public class AndTheCommandIsNotMvxCommand
+            {
+                [Test]
+                public void ShouldReturnNull()
+                {
+                    var notifyPropertyChanged = Substitute.For<INotifyPropertyChanged>();
+                    var command = Substitute.For<ICommand>();
+
+                    Assert.Null(command.RelayOn(notifyPropertyChanged));
+                }
+            }
+
+            [TestFixture]
+            public class AndTheCommandIsMvxCommand
+            {
+                // ReSharper disable once MemberCanBePrivate.Global
+                public interface ITestNotifyPropertyChanged : INotifyPropertyChanged
+                {
+                    string FirstProperty { get; }
+                    string SecondProperty { get; }
+                }
+
+                [Test]
+                public void ShouldNotReturnNull()
+                {
+                    var notifyPropertyChanged = Substitute.For<INotifyPropertyChanged>();
+                    var command = Substitute.For<IMvxCommand>();
+
+                    Assert.NotNull(command.RelayOn(notifyPropertyChanged));
+                }
+
+                [TestFixture]
+                public class AndSuitablePropertyChangedFired
+                {
+                    [Test]
+                    public void ShouldRaiseCanExecuteChanged()
+                    {
+                        var notifyPropertyChanged = Substitute.For<ITestNotifyPropertyChanged>();
+                        var command = Substitute.For<IMvxCommand>();
+
+                        var _ = command.RelayOn(notifyPropertyChanged, () => notifyPropertyChanged.FirstProperty);
+
+                        notifyPropertyChanged.PropertyChanged += Raise.Event<PropertyChangedEventHandler>(
+                            new PropertyChangedEventArgs(
+                                nameof(ITestNotifyPropertyChanged.FirstProperty)
+                            ));
+
+                        command.Received().RaiseCanExecuteChanged();
+                    }
+
+                    [TestFixture]
+                    public class AndSubscriptionIsDisposed
+                    {
+                        [Test]
+                        public void ShouldNotRaiseCanExecuteChanged()
+                        {
+                            var notifyPropertyChanged = Substitute.For<ITestNotifyPropertyChanged>();
+                            var command = Substitute.For<IMvxCommand>();
+
+                            var subscription = command.RelayOn(
+                                notifyPropertyChanged,
+                                () => notifyPropertyChanged.FirstProperty
+                            );
+
+                            subscription.Dispose();
+
+                            notifyPropertyChanged.PropertyChanged += Raise.Event<PropertyChangedEventHandler>(
+                                new PropertyChangedEventArgs(
+                                    nameof(ITestNotifyPropertyChanged.FirstProperty)
+                                ));
+
+                            command.DidNotReceive().RaiseCanExecuteChanged();
+                        }
+                    }
+                }
+
+                [TestFixture]
+                public class AndNotSuitablePropertyChangedFired
+                {
+                    [Test]
+                    public void ShouldRaiseCanExecuteChanged()
+                    {
+                        var notifyPropertyChanged = Substitute.For<ITestNotifyPropertyChanged>();
+                        var command = Substitute.For<IMvxCommand>();
+
+                        var _ = command.RelayOn(notifyPropertyChanged, () => notifyPropertyChanged.FirstProperty);
+
+                        notifyPropertyChanged.PropertyChanged += Raise.Event<PropertyChangedEventHandler>(
+                            new PropertyChangedEventArgs(
+                                nameof(ITestNotifyPropertyChanged.SecondProperty)
+                            ));
+
+                        command.DidNotReceive().RaiseCanExecuteChanged();
                     }
                 }
             }
--- SByte.MvvmCross.Extensions/SByte.MvvmCross.Extensions/CommandExtensions.cs
+++ SByte.MvvmCross.Extensions/SByte.MvvmCross.Extensions/CommandExtensions.cs
@@ -1,6 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using MvvmCross.Base;
 using MvvmCross.Commands;
+using MvvmCross.WeakSubscription;
 using SByte.Common.Extensions;
 
 namespace SByte.MvvmCross.Extensions
@@ -24,6 +30,39 @@ namespace SByte.MvvmCross.Extensions
             }
 
             return true;
+        }
+
+        public static IDisposable RelayOn(
+            this ICommand command,
+            INotifyPropertyChanged notifyPropertyChanged,
+            params Expression<Func<object>>[] properties
+        )
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            if (notifyPropertyChanged == null)
+            {
+                throw new ArgumentNullException(nameof(notifyPropertyChanged));
+            }
+
+            if (!(command is IMvxCommand mvxCommand))
+            {
+                return null;
+            }
+
+            return notifyPropertyChanged.WeakSubscribe((_, args) =>
+            {
+                if (properties.All(item =>
+                    notifyPropertyChanged.GetPropertyNameFromExpression(item) != args.PropertyName))
+                {
+                    return;
+                }
+
+                mvxCommand.RaiseCanExecuteChanged();
+            });
         }
     }
 }
