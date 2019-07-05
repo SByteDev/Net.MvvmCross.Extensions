242e6ce97bf314a43b475e2a9f55c8a8b7c0fb10
Denys Fiediaiev <fiediaiev@sbyte.dev>
2019-07-05 18:47:44 +0300

    Add some documentation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DotNet.MvvmCross.Extensions
+# Extensions for MvvmCross
+
+## ICommand Extensions
+1. `SafeExecuteAsync` safely invokes the command if this command can be executed in its current state. If this command is `IMvxAsyncCommand` it will be executed asynchronously.
+2. `RelayOn` calls `IMvxCommand.RaiseCanExecuteChanged` when corresponding property or properties changed.
--- SByte.MvvmCross.Extensions/SByte.MvvmCross.Extensions/CommandExtensions.cs
+++ SByte.MvvmCross.Extensions/SByte.MvvmCross.Extensions/CommandExtensions.cs
@@ -13,6 +13,13 @@ namespace SByte.MvvmCross.Extensions
 {
     public static class CommandExtensions
     {
+        /// <summary>
+        /// Safely invokes the command if this command can be executed in its current state.
+        /// If this command is <c>IMvxAsyncCommand</c> it will be executed asynchronously.
+        /// </summary>
+        /// <param name="command">The command itself.</param>
+        /// <param name="parameter">An optional parameter.</param>
+        /// <returns>True if the command was invoked and False otherwise.</returns>
         public static async Task<bool> SafeExecuteAsync(this ICommand command, object parameter = null)
         {
             if (!command.SafeCanExecute(parameter))
@@ -32,6 +39,17 @@ namespace SByte.MvvmCross.Extensions
             return true;
         }
 
+        /// <summary>
+        /// Calls <c>RaiseCanExecuteChanged</c> on <c>IMvxCommand</c>
+        /// when property from <paramref name="properties"/> changed.
+        /// </summary>
+        /// <param name="command">The target command.</param>
+        /// <param name="notifyPropertyChanged">A container for properties.</param>
+        /// <param name="properties">A list of properties to relay on.</param>
+        /// <returns><c>IDisposable</c> that represents subscription.
+        /// Can be disposed to stop listening for <c>PropertyChanged</c> event.
+        /// Returns <c>null</c> if <paramref name="command"/> is not <c>IMvxCommand</c></returns>
+        /// <exception cref="ArgumentNullException">If command or notifyPropertyChanged are <c>null</c>.</exception>
         public static IDisposable RelayOn(
             this ICommand command,
             INotifyPropertyChanged notifyPropertyChanged,
